df8c0240e1e5fcece50450ae173fce77ae158e94
Abhijit <abhijeetawari1402@gmail.com>
2023-02-27 15:17:37 +0530

    learn reducer and ref again and do remaining part

--- src/components/Store/CartProvider.js
+++ src/components/Store/CartProvider.js
@@ -1,10 +1,33 @@
-import CartContext from './cart-context';
-const CartProvider = props => {
-    const addItemtoContextHandler = item => {};
+import { useReducer } from 'react';
 
-    const removeItemtoContextHandler = item => {};
+import CartContext from './cart-context';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action ) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {items: updatedItems, totalAmount: updatedTotalAmount}
+    }
+    return defaultCartState;
+}
+
+const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemtoContextHandler = item => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemtoContextHandler = id => {
+        dispatchCartAction({type: 'REMOVE', id: id});
+    };
     const cartContext = {
-        items: [],
+        items: cartState.items ,
     totalAmount: 0,
     addItem: addItemtoContextHandler,
     removeItem: removeItemtoContextHandler
